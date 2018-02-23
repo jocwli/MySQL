b9fb6480f57609f017df29117b43aa32596bf793
jocwli <jcwahli@gmail.com>
2018-02-23 14:28:31 -0800

    Update 03_Basic select statements part2.sql

--- SQL_exercise02/03_Basic select statements part2.sql
+++ SQL_exercise02/03_Basic select statements part2.sql
@@ -41,48 +41,48 @@ WHERE list_price BETWEEN 30.00 AND 100.00;
 /*Range Operator*/
 SELECT order_id, order_date
 FROM Orders
-WHERE CONVERT(order_date, char(20)) BETWEEN "2015-03-01%" AND "2015-03-31%"
+WHERE CONVERT(order_date, char(20)) BETWEEN "2018-03-01%" AND "2018-03-31%"
 ORDER BY order_date DESC;
 
  /*QUERY 6b: Using Logical Operator for where the orders are during the month of March*/
 SELECT order_id, order_date
 FROM Orders
-WHERE CONVERT(order_date, char(20)) > "2015-03-01%" AND CONVERT(order_date, char(20)) <= "2015-03-31%" 
+WHERE CONVERT(order_date, char(20)) > "2018-03-01%" AND CONVERT(order_date, char(20)) <= "2018-03-31%" 
 ORDER BY order_date DESC;
  
  /*QUERY 6c: Search Pattern for where the orders are during the month of March*/
 SELECT order_id, order_date
 FROM Orders
-WHERE CONVERT(order_date, char(20)) LIKE "2015-03-__%"
+WHERE CONVERT(order_date, char(20)) LIKE "2018-03-__%"
 ORDER BY order_date DESC;
  
-/*QUERY 7:*/
+/*QUERY 7: Display orders from the orders table where orders shipped after Jan 10th 2018*/
 SELECT order_id, ship_date
 FROM Orders
-WHERE ship_date > "2015-01-10%";
+WHERE ship_date > "2018-01-10%";
  
-/*QUERY 8*/
+/*QUERY 8: Display orders from the orders table where orders placed on before or after Feb 28th 2018*/
 SELECT order_id, order_date
 FROM Orders
-WHERE order_date <= "2015-04-01%";
+WHERE order_date <= "2018-02-28%";
  
-/*QUERY 9*/
+/*QUERY 9: Display the products where the product name contains E in the second letter and L in the fourth letter*/
 SELECT product_id, product_code, product_name
 FROM Product
 WHERE product_name LIKE "_E_L%"
 ORDER BY product_code DESC;
  
-/*QUERY 10*/
+/*QUERY 10: Display the products in category 10 or 20, with vendor_id in 1 or 2, and with a list price less than $1000*/
 SELECT product_id, category_id, vendor_id, list_price
 FROM Product
-WHERE category_id IN(11,16) AND vendor_id IN(1,2) AND list_price <1100;
+WHERE category_id IN(10,20) AND vendor_id IN(1,2) AND list_price <1000;
  
-/*QUERY 11*/
+/*QUERY 11: Display all orders that have not shipped for at least 3 days*/
 SELECT *
 FROM Orders
 WHERE DATEDIFF(order_date, ship_date) <=3;
  
-/*QUERY 12*/
+/*QUERY 12: Display all orders that have shipped*/
 SELECT order_id, customer_id, order_date
 FROM Orders
 WHERE ship_date IS NOT NULL;
